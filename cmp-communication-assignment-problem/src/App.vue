4f0a503fd36e1e1bfa91a55105b33df6484cb2ff
Deivison Cardoso <deivisondc@gmail.com>
2023-10-24 11:46:48 -0300

    assignment 6 - module 8

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Assignment 6</h1>
+
+  <active-user :user="activeUser" />
+  <user-data @set-user="setActiveUser" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeUser: {
+        name: "",
+        age: 0,
+      },
+    };
+  },
+  methods: {
+    setActiveUser(user) {
+      this.activeUser = { ...user };
+    },
+  },
+};
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="submit">
+    <div>
+      <label>Name</label>
+      <input v-model="user.name" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input type="number" v-model="user.age" />
+    </div>
+
+    <button>Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["set-user"],
+  data() {
+    return {
+      user: {
+        name: "",
+        age: 0,
+      },
+    };
+  },
+  methods: {
+    submit() {
+      this.$emit("set-user", this.user);
+    },
+  },
+};
+</script>
