a94c3b509b58787627af7533b677d0cf3532b2e5
Deivison Cardoso <deivisondc@gmail.com>
2023-11-02 08:58:11 -0300

    module 9 - fragment (implicit)

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="setGoal">Set Goal</button>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
 
-    <Teleport to="body">
-      <ErrorAlert v-if="inputIsInvalid">
-        <h2>Input is invalid!</h2>
-        <p>Please enter at least a few characters...</p>
-        <button @click="confirmError">OK</button>
-      </ErrorAlert>
-    </Teleport>
-  </div>
+  <Teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">OK</button>
+    </ErrorAlert>
+  </Teleport>
 </template>
 
 <script>
